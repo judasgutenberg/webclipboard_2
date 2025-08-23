5f0c139d8d1890849ee114632c9519a085dcfae8
Gus Mueller <gus@asecular.com>
2025-08-23 13:01:12 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ $username = "weather";
 $database = "weathertron";
 $password = "fffgggfffgggfffggg";
 $timezone = "America/New_York";
-$storagePassword = "???????????????"; 
 $encryptionPassword = "rrttrrttrrtt";
 $cookiename = "WebClipBoard";
 $timezone = "America/New_York";
